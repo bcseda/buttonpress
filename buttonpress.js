6ee16c98011451050efe9e875f7e4eeadecd575c
bcseda <bcseda@gmail.com>
2014-12-20 13:48:43 -0500

    adds height and width. adds some styling

--- buttonpress.js
+++ buttonpress.js
@@ -2,31 +2,46 @@ function buttonViewModel() {
   self = this;
   self.text = ko.observable('Text');
   self.fonts = ko.observableArray([
-    {optText: 'Arial', optVal: 'Arial, sans-serif'},
-    {optText: 'Times New Roman', optVal: '"Times New Roman", serif'}
-  ]);
-  self.selectedFont = ko.observable('Arial')
+  {optText: 'Arial', optVal: 'Arial, sans-serif'},
+{optText: 'Times New Roman', optVal: '"Times New Roman", serif'}
+]);
+self.selectedFont = ko.observable('Arial');
 
-  //Border
-  self.borderWidth = ko.observable(1);
+//Sizing
+self.width = ko.observable(100);
+self.height = ko.observable(20);
 
-  //Colors
-  self.fontColor = ko.observable('#000000')
-  self.baseColor = ko.observable('#cccccc')
-  self.borderColor = ko.observable('#333333')
-  self.gradientColor = ko.observable('#cccccc')
+//Border
+self.borderWidth = ko.observable(1);
 
-  self.btnStyle = ko.computed(function(){
-    var style = {
-      fontFamily: self.selectedFont,
-      borderWidth: self.borderWidth()+'px',
-      borderStyle: "solid",
-      color: self.fontColor,
-      backgroundColor: self.baseColor,
-      borderColor: self.borderColor
-    };
-    return style;
-  },this);
+//Colors
+self.fontColor = ko.observable('#000000');
+self.baseColor = ko.observable('#cccccc');
+self.borderColor = ko.observable('#333333');
+self.gradientColor = ko.observable('#cccccc');
+
+function px(val){
+  return val + 'px';
+}
+
+self.btnStyle = ko.computed(function(){
+
+  var style = {
+    //hard coded
+    textAlign: 'center',
+
+    width: px(self.width()),
+    height: px(self.height()) ,
+    lineHeight: px(self.height()),
+    fontFamily: self.selectedFont,
+    borderWidth: self.borderWidth()+'px',
+    borderStyle: "solid",
+    color: self.fontColor,
+    backgroundColor: self.baseColor,
+    borderColor: self.borderColor
+  };
+  return style;
+},this);
 }
 
 $(function(){
